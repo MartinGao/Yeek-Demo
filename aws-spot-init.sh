1c1bd02b83e1d62930c958c9d7d3902bda32c3f0
Martin Gao <Marting.Gao@Gmail.com>
2018-04-28 18:42:27 -0700

    Update aws-spot-init.sh

--- aws-spot-init.sh
+++ aws-spot-init.sh
@@ -3,3 +3,10 @@ git pull
 yarn
 export PATH='$(yarn global bin):$PATH'
 pm2 start fakeIndex.js
+
+INSTANCE_ID=`ec2metadata --instance-id`
+INSTANCE_TYPE=`ec2metadata --instance-type`
+LOCAL_IP=`ec2metadata --local-ipv4`
+PUBLIC_IP=`ec2metadata --public-ipv4`
+AMI_ID=`ec2metadata --ami-id`
+wget -qO- "http://52.52.177.194:3008/ip/create?instanceId=$INSTANCE_ID&instanceType=$INSTANCE_TYPE&localIp=$LOCAL_IP&publicIp=$PUBLIC_IP&amiId=$AMI_ID&yeek=Yeek-Fetch-New"
